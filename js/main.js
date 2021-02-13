79759254f5fad218970a98775d9fdd2d8a84b6d4
Bogdan-jsx <sbogdan3755@gmail.com>
2021-02-13 19:48:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
